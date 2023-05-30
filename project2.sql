006c1bbe10753943b8de449fb2e0fbb929b3be74
James Preston <preston_104@hotmail.com>
2023-05-30 11:25:33 +1000

    re-adjusting the sql file to take inputs from data types in form

--- project2.sql
+++ project2.sql
@@ -5,7 +5,7 @@ CREATE TABLE if not exists meetings (
    meeting_id SERIAL PRIMARY KEY,
    owner_name TEXT NOT NULL,
    meeting_subject TEXT NOT NULL,
-   meeting_time VARCHAR(255) NOT NULL,
+   meeting_time TEXT NOT NULL,
    location TEXT NOT NULL CHECK (location IN ('Meeting Room 1', 'Meeting Room 2', 'Meeting Room 3', 'Meeting Room 4', 'Meeting Room 5')),
    attendees TEXT[]
 );
